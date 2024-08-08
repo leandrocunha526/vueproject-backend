be380a7b7f0d7b1744c0b79666ffec040a2bc457
Leandro Cunha <leandrocunha016@gmail.com>
2024-08-08 19:48:49 -0300

    chore: Added detail in commentary

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -26,11 +26,14 @@ export class AuthService {
             expiresIn: '1h',
         });
         /*
-           Here, I was given the option to not return the token,
-           but it is optional if I want to change the authentication
-           to a token instead of a cookie stored in session storage.
-           The front-end is integrated in this way.
-           See user/user.controller.ts for more details.
+        Note: Returning the token here is optional. If the frontend is configured to
+        use authentication cookies, the token may be sent as an HttpOnly cookie instead of
+        being returned directly. Refer to `user/user.controller.ts` for specifics on the
+        implementation and how authentication cookies are being utilized.
+        The login method receives the token from the AuthService.
+        It sets the token as an HttpOnly cookie (auth-cookie) in the response using res.cookie.
+        So, while the AuthService generates the token, the UserController is responsible for
+        setting this token as a cookie in the client browser and storing in session storage.
         */
         return {
             token: token,
